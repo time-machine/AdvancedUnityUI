5ce55b551b122c722cbcf320daaad55cd80a487e
Tery Lim <kumabotz@gmail.com>
2020-01-27 19:38:32 +1300

    Create start window

---
+++ Assets/Scripts/Windows/GenericWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Windows
+{
+    public class GenericWindow : MonoBehaviour
+    {
+        public GameObject FirstSelected;
+
+        protected EventSystem EventSystem => GameObject.Find("EventSystem").GetComponent<EventSystem>();
+
+        public virtual void OnFocus()
+        {
+            EventSystem.SetSelectedGameObject(FirstSelected);
+        }
+
+        protected virtual void Display(bool value)
+        {
+            gameObject.SetActive(value);
+        }
+
+        public virtual void Open()
+        {
+            Display(true);
+            OnFocus();
+        }
+
+        public virtual void Close()
+        {
+            Display(false);
+        }
+
+        protected virtual void Awake()
+        {
+            Close();
+        }
+    }
+}
---
+++ Assets/Scripts/Windows/StartWindow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Windows
+{
+    public class StartWindow : GenericWindow
+    {
+        public Button ContinueButton;
+
+        protected override void Awake()
+        {
+        }
+
+        void Start()
+        {
+            Open();
+        }
+
+        public override void Open()
+        {
+            var canContinue = true;
+            ContinueButton.gameObject.SetActive(canContinue);
+            if (ContinueButton.gameObject.activeSelf)
+            {
+                FirstSelected = ContinueButton.gameObject;
+            }
+            base.Open();
+        }
+
+        public void NewGame()
+        {
+            Debug.Log("New Game Pressed");
+        }
+
+        public void Continue()
+        {
+            Debug.Log("Continue Pressed");
+        }
+
+        public void Options()
+        {
+            Debug.Log("Options Pressed");
+        }
+    }
+}

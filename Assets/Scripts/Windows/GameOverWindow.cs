9e101a663d5fbb273418206e07a8e0eb8cdffd88
Tery Lim <kumabotz@gmail.com>
2020-02-15 13:03:59 +1300

    Add game over scene

---
+++ Assets/Scripts/Windows/GameOverWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Scripts.Windows
+{
+    public class GameOverWindow : GenericWindow
+    {
+        protected override void Awake()
+        {
+        }
+
+        void Start()
+        {
+            Open();
+        }
+    }
+}
